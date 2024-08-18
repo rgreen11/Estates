d1f74c1162f10cc1ca5686c81b50ee01c5623185
Rich G <richg@richs-mbp.mynetworksettings.com>
2024-08-18 12:12:32 -0400

    input fix

--- view/src/components/Form.tsx
+++ view/src/components/Form.tsx
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/Form.css";
 
+const InputForm = () => {
+  const customerInfo = [
+    "name",
+    "email",
+    "phone_number",
+    // "address",
+    // "has_realtor",
+    // "brokerage",
+  ];
+  const [texts, setTexts] = useState({});
 
+  const handleChange = (event, type) => {
+    if (event.target.value) {
+      texts[type] = event.target.value
+       setTexts({...texts});
+       console.log(texts)
+    }
+  };
+
+  return (
+    <>
+      {customerInfo.map((type, idx) => {
+        return (
+          <input
+            id={`user${type}Input`}
+            type="type"
+            key={idx}
+            onChange={(e) => handleChange(e, type)}
+          />
+        );
+      })}
+    </>
+  );
+};
 
 const Form = ({ title }: { title: string }) => {
-
-  const handleChange = (event) =>{
-  
-  }
-
-
   const submitData = (event) => {
     event.preventDefault();
     const {
       usernameInput,
       useremailInput,
       userphoneNumberInput,
-
       userhasRealtorInput,
       userbrokerageInput,
     } = event.currentTarget.elements;
 
-    if(!usernameInput || !useremailInput || !userphoneNumberInput){
-
+    if (!usernameInput || !useremailInput || !userphoneNumberInput) {
     }
-  
+
     return fetch("http://localhost:8080/users", {
       method: "post",
       mode: "no-cors", // I intsalled cors so I may not need this
@@ -37,16 +62,12 @@ const Form = ({ title }: { title: string }) => {
       });
   };
 
+  // <form className="form_container" onSubmit={submitData}></form>
   return (
-    <form className="form_container" onSubmit={submitData}>
-      <input id="usernameInput" type="name" onChange={handleChange} />
-      <input id="useremailInput" type="email" onChange={handleChange} />
-      <input id="userphoneNumberInput" type="text" onChange={handleChange} />
-      {/* <input id="userHasRealtorInput" type="radio" onChange={handleChange} />
-      <input id="userNoRealtorInput" type="radio" onChange={handleChange} /> */}
-      <input id="userbrokerageInput" type="text" onChange={handleChange} />
+    <div className="form_container">
+      <InputForm />
       <button type="submit">{title}</button>
-    </form>
+    </div>
   );
 };
 
